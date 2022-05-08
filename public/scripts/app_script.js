aac3db4cb7d337b703f70fd43beef1cd5184252e
naimulh247 <naimulh247@gmail.com>
2022-05-08 13:22:32 -0400

    Add js scripts

---
+++ public/scripts/app_script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const overlay = document.querySelector(".overlay")
+
+hamburger_menu.addEventListener("click", () => {
+    container.classList.toggle("active");
+});
